9a2f17ded4ab8831dc40f8b85683a96a3063d0e7
PEDRO HENRIQUE NUNES ZANETTE <pedro_zanette@estudante.sesisenai.org.br>
2024-08-23 11:54:59 -0300

    Start

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 import { fastify } from 'fastify'
+import { DatabasePostgres } from './database-postgres.js';
 
 const server = fastify();
 
-server.get('/', () => {
-  return 'Brunna';
+const database = new DatabasePostgres;
+
+// database.create(); // POST
+// database.read(); // GET
+// database.update(); // PUT
+// database.delete(); // DELETE
+
+//Endpoints
+server.get('/', async () => {
+  const artigos = database.read();
+  return 'Frango do G';
+});
+
+server.get('/senai', () => {
+  return 'Monopólio do G';
 });
 
 server.listen({
